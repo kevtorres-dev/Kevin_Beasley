eb3eaa1af101f48723ae2d91f334aa1dd5ccb0e2
Kevin Torres <kevintorresdev@gmail.com>
2024-10-18 21:40:12 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+import {src, dest, watch, series} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+import terser from 'gulp-terser'
+
+export function js(done) {
+    src('src/js/app.js')
+        .pipe(terser())
+        .pipe(dest('build/js'))
+    done()
+}
+
+export function css(done) {
+    src('src/scss/app.scss', {sourcemaps: true})
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(dest('build/css', {sourcemaps: '.'}))
+    
+    done()
+}
+
+export function dev() { 
+    watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
+}
+
+export default series(js, css, dev)
